ac1398f15130c98a9cf8e1558f7db80f7fff01e7
Lamiya Gafarli <lamiyagafarli@gmail.com>
2023-12-20 00:20:33 +0400

    Add project files.

---
+++ LearnDSAlgorithms/BinaryLinearSearch.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnDSAlgorithms
+{
+    public class BinaryLinearSearch
+    {
+        public int binarysearch(int[] A, int key, int l, int r)
+        {
+            if (l > r)
+                return -1;
+            else
+            {
+                int mid = (l + r) / 2;
+                if (key == A[mid])
+                    return mid;
+                else if (key < A[mid])
+                    return binarysearch(A, key, l, mid - 1);
+                else if (key > A[mid])
+                    return binarysearch(A, key, mid + 1, r);
+            }
+            return -1;
+        }
+        public int binarysearchIterative(int[] A, int n, int key)
+        {
+            int l = 0;
+            int r = n - 1;
+            while (l <= r)
+            {
+                int mid = (l + r) / 2;
+                if (key == A[mid])
+                    return mid;
+                else if (key < A[mid])
+                    r = mid - 1;
+                else if (key > A[mid])
+                    l = mid + 1;
+            }
+            return -1;
+        }
+      
+
+        public int BinarySearchTwoDimension(int[,] A, int key)
+        {
+            int l = 0;
+            int r = A.Length - 1;
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+                    int middle = (l + r) / 2;
+                    if (A[i, j] == middle)
+                    {
+                        return middle;
+                    }
+                    else if (key < A[i, j])
+                    {
+                        r = middle - 1;
+                    }
+
+                    else if (key > A[i, j])
+                    {
+                        l = middle + 1;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public int binarysearchrecursive(int[] A, int key, int l, int r)
+        {
+            if (l > r)
+            {
+                return -1;
+            }
+            else
+            {
+                int mid = (l + r) / 2;
+                if (A[mid] == key)
+                {
+                    return mid;
+                }
+                else if (key < A[mid])
+                {
+                    return binarysearchrecursive(A, key, l, mid - 1);
+                }
+                else if (key > A[mid])
+                {
+                    return binarysearchrecursive(A, key, mid + 1, r);
+                }
+            }
+            return -1;
+        }
+
+        //LinearSearch
+        public int linearsearch(int[] A, int n, int key)
+        {
+            int index = 0;
+            while (index < n)
+            {
+                if (A[index] == key)
+                    return index;
+                index = index + 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ LearnDSAlgorithms/Recursion.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnDSAlgorithms
+{
+    public class Recursion
+    {
+        public void CalculateTail(int n)
+        {
+            if (n>0)
+            {
+                int k = n * n;
+                Console.WriteLine(k);
+                CalculateTail(n-1);
+            }    
+        }
+
+        public void CalculateHead(int n)
+        {
+            if (n > 0)
+            {
+                int k = n * n;
+                Console.WriteLine(k);
+                CalculateHead(n - 1);
+            }
+        }
+
+        public void CalculateTree(int n)
+        {
+            if (n > 0)
+            {
+                CalculateTree(n - 1);
+                int k = n * n;
+                Console.WriteLine(k);
+                CalculateTree(n - 1);
+            }
+        }
+
+
+        ///1+2+3+4+5+5...+n
+        public int sumrecursion(int n) {
+            if (n==0)
+            {
+                return 1;
+            }
+            return sumrecursion(n-1)*n;
+        }
+
+        public int LinearSearch(int[]A,int key)
+        {
+            int index = 0;
+            while (index<A.Length)
+            {
+                if (A[index]==key)
+                {
+                    return index;
+                }
+                index = index + 1;
+            }
+            return -1;
+        }
+
+        //Find Factorial
+        public int factorialiterative(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+                fact = fact * i;
+            return fact;
+        }
+
+        public int factorialrecursive(int n)
+        {
+            if (n == 0)
+                return 1;
+            return factorialrecursive(n - 1) * n;
+        }
+    }
+}
